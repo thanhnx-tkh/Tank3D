1fdad4d026cef38af0e93ad1a8e685aaca529ea2
thanhnx <thanhnx.tkh@gmail.com>
2024-10-17 17:01:18 +0700

    Update Map

--- Assets/_UI/Scripts/UI/GameManager.cs
+++ Assets/_UI/Scripts/UI/GameManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.Events;
-public enum GameState{
+public enum GameState
+{
     MainMenu,
     GamePlay,
     Win,
@@ -19,30 +20,18 @@ public class GameManager : Singleton<GameManager>
     // Start is called before the first frame update
     protected void Awake()
     {
-        //base.Awake();
-        Input.multiTouchEnabled = false;
-        Application.targetFrameRate = 60;
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
-
-        int maxScreenHeight = 1280;
-        float ratio = (float)Screen.currentResolution.width / (float)Screen.currentResolution.height;
-        if (Screen.currentResolution.height > maxScreenHeight)
-        {
-            Screen.SetResolution(Mathf.RoundToInt(ratio * (float)maxScreenHeight), maxScreenHeight, true);
-        }
-
         ChangeState(GameState.MainMenu);
 
         UIManager.Ins.OpenUI<MianMenu>();
     }
     public static void ChangeState(GameState state)
     {
-       gameState = state;
+        gameState = state;
     }
 
     public static bool IsState(GameState state)
     {
-       return gameState == state;
+        return gameState == state;
     }
-  
+
 }
